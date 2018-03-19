558ccfd0373254f43a9a7656ddc7e3c90c25d70f
oym-dev07 <oym-dev07@OYM-DEV07>
2018-03-19 12:56:37 -0300

    Añadido menú vertical con posibilidad de intercambiar mediante JQuery - 180319_CC

--- public_html/js/js.js
+++ public_html/js/js.js
@@ -1,5 +1,33 @@
 /*archivo con las funciones en JavaScript
  * @author Cristhiandavx*/
+/*Utilizando JQuery*/
 $(document).ready(function(){
-    
+    $("#configMenu-V").click(function(){
+        console.log("cambiar al menu vertical");
+        $("#vertical-nav, #horizontal-group").toggleClass("d-lg-none");
+        $("#content").toggleClass("col-lg-10");
+        $("#horizontal-group").toggleClass("d-lg-flex", "d-none");
+    });
+    $("#configMenu-H").click(function(){
+        console.log("cambiar al menu horizontal");
+        $("#vertical-nav, #horizontal-group").toggleClass("d-lg-none");
+        $("#content").toggleClass("col-lg-10");
+        $("#horizontal-group").toggleClass("d-lg-flex", "d-none");
+    });
+    $("#configIcon-E").click(function(){
+        console.log("expandir menu");
+        $(".etiqueta").toggleClass("d-lg-block");
+        $("#content").toggleClass("col-lg-7");
+        $("#vertical-nav").toggleClass("text-center");
+        $("#configIcon-E").toggleClass("d-none");
+        $("#configIcon-C").toggleClass("d-none");
+    });
+    $("#configIcon-C").click(function(){
+        console.log("expandir menu");
+        $(".etiqueta").toggleClass("d-lg-block");
+        $("#content").toggleClass("col-lg-7");
+        $("#vertical-nav").toggleClass("text-center");
+        $("#configIcon-C").toggleClass("d-none");
+        $("#configIcon-E").toggleClass("d-none");
+    });
 });
